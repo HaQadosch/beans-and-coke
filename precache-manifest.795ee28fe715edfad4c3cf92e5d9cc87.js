daf4adb8b100d718605ea3acaed501e529c7bd78
Xavier Brinon <xavier.brinon@ecs.co.uk>
2020-10-02 21:45:44 +0100

    Updates

---
+++ precache-manifest.795ee28fe715edfad4c3cf92e5d9cc87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ee1c4b01e8194e648f0a55105faf21",
+    "url": "/beans-and-coke/index.html"
+  },
+  {
+    "revision": "42438ff35019c775c9a4",
+    "url": "/beans-and-coke/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "6811ac7f55b031500087",
+    "url": "/beans-and-coke/static/js/2.6b892acd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beans-and-coke/static/js/2.6b892acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42438ff35019c775c9a4",
+    "url": "/beans-and-coke/static/js/main.bb93a69d.chunk.js"
+  },
+  {
+    "revision": "75a8acac9b1ec9b15296",
+    "url": "/beans-and-coke/static/js/runtime-main.b4d874ae.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/beans-and-coke/static/media/logo.8dad8028.svg"
+  }
+]);
