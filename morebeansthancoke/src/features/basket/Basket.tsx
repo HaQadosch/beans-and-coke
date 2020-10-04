62075e87b9b6eefa93aa3a1830d1a1e3f1e3292a
HaQadosch Berraka <haqadosch@pm.me>
2020-10-04 23:18:17 +0100

    React: Basket FC

--- morebeansthancoke/src/features/basket/Basket.tsx
+++ morebeansthancoke/src/features/basket/Basket.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import './basket.css'
+import { removeItem, selectBasket } from './basketSlice'
 
 interface IBasket {
 
 }
 
 export const Basket: React.FC<IBasket> = () => {
+  const { basket } = useSelector(selectBasket)
+  const dispatch = useDispatch()
 
   return (
-    <section className="basket nes-table-responsive"></section>
+    <section className="basket nes-table-responsive">
+      <div className="nes-container is-rounded with-title">
+        <p className="title">Basket</p>
+        <div className="lists">
+          <ul className="nes-list is-disc">
+            { basket.map(({ uuid, name, pricePerKg, pricePerUnit }) => (
+              <li key={ uuid }>
+                <button type="button" onClick={ () => dispatch(removeItem(uuid)) } className="nes-btn"><i className="nes-icon close is-small"></i></button>
+                { name } { ' ' }
+                <span className="nes-text is-success">{ pricePerKg || pricePerUnit }</span>
+              </li>
+            )) }
+            <li>----</li>
+            <li>Sub-total <span className="nes-text is-success">3.30</span></li>
+          </ul>
+        </div>
+      </div>
+    </section>
   )
 }
--- morebeansthancoke/src/features/products/Products.test.tsx
+++ morebeansthancoke/src/features/products/Products.test.tsx
@@ -5,46 +5,48 @@ import { store, testState } from '../../app/store'
 import { ProductCard, Products } from './Products'
 import { selectProducts } from './productSlice'
 
-test('renders the list of products available in the store', () => {
-  const { debug, getAllByText, getByText } = render(
-    <Provider store={ store }>
-      <Products />
-    </Provider>
-  )
+describe('<Product />', () => {
+  test('renders the list of products available in the store', () => {
+    const { debug, getAllByText, getByText } = render(
+      <Provider store={ store }>
+        <Products />
+      </Provider>
+    )
 
-  expect(getAllByText(/add to basket/i)).toHaveLength(3)
-  expect(getByText(/Beans/i, { selector: 'h2' }))
-  expect(getByText(/Coke/i, { selector: 'h2' }))
-  expect(getByText(/Oranges/i, { selector: 'h2' }))
-})
-
-test('renders the Beans details', async () => {
-  const { products } = selectProducts({
-    ...testState,
-    products: {
-      products: [
-        {
-          sku: '1',
-          name: 'Beans',
-          description: 'is a perfect addition to your dishes. Bring protein, fiber and flavor to your favorite recipes.',
-          pricePerKg: null,
-          pricePerUnit: 0.5,
-          picture: 'Beans.png'
-        },
-      ]
-    }
+    expect(getAllByText(/add to basket/i)).toHaveLength(3)
+    expect(getByText(/Beans/i, { selector: 'h2' }))
+    expect(getByText(/Coke/i, { selector: 'h2' }))
+    expect(getByText(/Oranges/i, { selector: 'h2' }))
   })
 
-  const { getByText, getByAltText, container } = render(
-    <ProductCard { ...products[0] } />
-  )
+  test('renders the Beans details', async () => {
+    const { products } = selectProducts({
+      ...testState,
+      products: {
+        products: [
+          {
+            sku: '1',
+            name: 'Beans',
+            description: 'is a perfect addition to your dishes. Bring protein, fiber and flavor to your favorite recipes.',
+            pricePerKg: null,
+            pricePerUnit: 0.5,
+            picture: 'Beans.png'
+          },
+        ]
+      }
+    })
 
-  await waitFor(() => {
-    expect(container.querySelector('img')?.src).toMatch(/Beans.png/i)
+    const { getByText, getByAltText, container } = render(
+      <ProductCard { ...products[0] } />
+    )
+
+    await waitFor(() => {
+      expect(container.querySelector('img')?.src).toMatch(/Beans.png/i)
+    })
+    expect(getByText(/Beans/i, { selector: 'h2' }))
+    expect(getByText(/add to basket/i))
+    expect(getByText(/0.5/))
+    expect(getByText(/is a perfect addition to your dishes/i))
+    expect(getByAltText(/Beans/i))
   })
-  expect(getByText(/Beans/i, { selector: 'h2' }))
-  expect(getByText(/add to basket/i))
-  expect(getByText(/0.5/))
-  expect(getByText(/is a perfect addition to your dishes/i))
-  expect(getByAltText(/Beans/i))
 })
