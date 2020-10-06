31e0d96527173b3e6ab9a7a189129be2d8ab3407
HaQadosch Berraka <haqadosch@pm.me>
2020-10-06 21:45:34 +0100

    fix: round the price with 2 decimals

--- morebeansthancoke/src/features/basket/Basket.tsx
+++ morebeansthancoke/src/features/basket/Basket.tsx
@@ -26,10 +26,24 @@ export const Basket: React.FC<IBasket> = () => {
             )) }
             <li>----</li>
             <li>Sub-total <span className="nes-text is-success">{
-              basket.reduce((acc, curr) => (curr.pricePerKg || curr.pricePerUnit || 0) + acc, 0)
+              basket.reduce((acc, curr) => (curr.pricePerKg || curr.pricePerUnit || 0) + acc, 0).toFixed(2)
             }</span></li>
           </ul>
         </div>
+      </div>
+      <div className="nes-container is-rounded with-title">
+        <p className="title">Savings</p>
+        <div className="lists">
+          <ul className="nes-list is-circle">
+            <li>Beans 3 for 2 <span className="nes-text is-success">-0.50</span></li>
+            <li>Coke 2 for #1 <span className="nes-text is-success">-0.40</span></li>
+            <li>----</li>
+            <li>Total savings <span className="nes-text is-success">-0.90</span></li>
+          </ul>
+        </div>
+      </div>
+      <div className="nes-container is-dark is-rounded">
+        <p>Total to pay: <span className="nes-text is-success">2.40</span></p>
       </div>
     </section>
   )
